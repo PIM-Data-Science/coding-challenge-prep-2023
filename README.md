f16dd507460afed23c5b552348277be9ffab449a
PIM-Data-Science <129489716+PIM-Data-Science@users.noreply.github.com>
2023-03-31 13:11:56 +0000

    chart plotted

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Prescient Coding Challenge Prep Material
+# Prescient Coding Challenge preparatory Material
 
-Prescient coding challenge prep material
+The content of the preparatory material is to introduce you to time series data and basic methods of working/viewing time series data.
+
+## Instructions
+
+Follow the content in either `prep.py` or `prep.ipynb` files. You may close this repo and work through the content. The recomendation is to clone the repo and open the folder location in VS Code. You may also start a GitHub Workspace to work through the content.
+
+The Python files are executed assuming a relative path or your current working directory is the repo folder.
+
+## 
--- prep.py
+++ prep.py
@@ -1,15 +1,37 @@
 # %%
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+
+import plotly.express as px
 
 # %%[markdown]
 
-# # Time Series Data
+# ### Time Series Data
 
-# Financial Data
+# Financial and economic data is mostly displayed and stored as time series. The time series data in the `csv` file is in long format
 
 # %%
 
-df = pd.DataFrame([1])
-df
+# read data
+df_prices = pd.read_csv('price_data.csv')
+df_prices
+
+# %%
+
+print('---> csv data in wide form')
+df_prices_wide = df_prices.pivot(index='date', columns='stock', values='price')
+df_prices_wide
+
+
+# %%
+
+# the 3 stocks price series diplayed on the same chart
+fig1 = px.line(data_frame=df_prices, x='date', y='price', line_group='stock', color='stock', title='Stock Price Series')
+fig1.show()
+
+# %%[markdown]
+
+# ### Investing Oversimplified
+
+# Not sure what to say in summary here
